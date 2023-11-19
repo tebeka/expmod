7e583b7f584d6eb9a8b8aab2d515bcc7deb2873c
Miki Tebeka <miki@353solutions.com>
2023-11-19 14:10:10 +0200

    testing the executable

--- main_test.go
+++ main_test.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"bytes"
+	"context"
 	"os"
+	"os/exec"
+	"path"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 	"golang.org/x/mod/modfile"
@@ -72,6 +77,92 @@ require (
 		t.Run(req.Mod.Path, func(t *testing.T) {
 			v := ignored(req)
 			require.Equal(t, ignores[i], v)
+		})
+	}
+}
+
+func testCtx(t *testing.T) (context.Context, context.CancelFunc) {
+	deadline, ok := t.Deadline()
+	if ok {
+		return context.WithDeadline(context.Background(), deadline)
+	}
+
+	return context.WithTimeout(context.Background(), 3*time.Second)
+}
+
+func build(t *testing.T) string {
+	exe := path.Join(t.TempDir(), "expmod")
+	t.Logf("exe: %q", exe)
+	ctx, cancel := testCtx(t)
+	defer cancel()
+
+	err := exec.CommandContext(ctx, "go", "build", "-o", exe).Run()
+	require.NoError(t, err, "build")
+	return exe
+}
+
+var testMod = "testdata/go.mod"
+var exeExpected = `github.com/sahilm/fuzzy v0.1.0:
+	Go library that provides fuzzy string matching optimized for filenames and code symbols in the style of Sublime Text, VSCode, IntelliJ IDEA et al.
+github.com/stretchr/testify v1.8.4:
+	A toolkit with common assertions and mocks that plays nicely with the standard library
+`
+
+func TestExe(t *testing.T) {
+	exe := build(t)
+
+	ctx, cancel := testCtx(t)
+	defer cancel()
+
+	var buf bytes.Buffer
+	cmd := exec.CommandContext(ctx, exe, testMod)
+	cmd.Stdout = &buf
+	err := cmd.Run()
+
+	require.NoError(t, err, "run")
+	require.Equal(t, exeExpected, buf.String())
+}
+
+func TestExeStdin(t *testing.T) {
+	exe := build(t)
+
+	ctx, cancel := testCtx(t)
+	defer cancel()
+
+	file, err := os.Open(testMod)
+	require.NoError(t, err, "open mod")
+	defer file.Close()
+
+	var buf bytes.Buffer
+	cmd := exec.CommandContext(ctx, exe)
+	cmd.Stdin = file
+	cmd.Stdout = &buf
+
+	err = cmd.Run()
+	require.NoError(t, err, "run")
+	require.Equal(t, exeExpected, buf.String())
+}
+
+var flagCases = []struct {
+	flag     string
+	fragment string
+}{
+	{"-version", "version"},
+	{"-help", "usage"},
+}
+
+func TestExeFlags(t *testing.T) {
+	exe := build(t)
+
+	for _, tc := range flagCases {
+		t.Run(tc.flag, func(t *testing.T) {
+			ctx, cancel := testCtx(t)
+			defer cancel()
+
+			cmd := exec.CommandContext(ctx, exe, tc.flag)
+			out, err := cmd.CombinedOutput()
+			require.NoError(t, err, "run")
+			require.Contains(t, string(out), tc.fragment)
 		})
 	}
 }
