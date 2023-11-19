fe00cd222c9e6c74df50c963eb1befd8b486f359
Miki Tebeka <miki@353solutions.com>
2023-11-19 16:11:47 +0200

    test hitting site

--- pkg_test.go
+++ pkg_test.go
@@ -7,6 +7,10 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+const (
+	zapRepo = "https://github.com/uber-go/zap"
+)
+
 func Test_parsePkgHTML(t *testing.T) {
 	file, err := os.Open("testdata/pkg-zap.html")
 	require.NoError(t, err, "read HTML")
@@ -14,5 +18,18 @@ func Test_parsePkgHTML(t *testing.T) {
 
 	repo, err := parsePkgHTML(file)
 	require.NoError(t, err, "parse HTML")
-	require.Equal(t, "https://github.com/uber-go/zap", repo)
+	require.Equal(t, zapRepo, repo)
+}
+
+func Test_repoFromPkg(t *testing.T) {
+	if os.Getenv("CI") != "" {
+		t.Skip("In CI")
+	}
+
+	ctx, cancel := testCtx(t)
+	defer cancel()
+
+	repo, err := repoFromPkg(ctx, "go.uber.org/zap")
+	require.NoError(t, err, "parse HTML")
+	require.Equal(t, zapRepo, repo)
 }
